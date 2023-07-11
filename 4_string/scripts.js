74be70b1d5a6a440b457a3b5f749d99c3918e636
Luciano Bittencourt <lucianobitt@hotmail.com>
2023-07-11 16:00:18 -0300

    estudos

---
+++ 4_string/scripts.js
@@ -0,0 +1,5 @@
+console.log(typeof "Oi! Meu nome é Luciano Bittencourt");
+console.log(typeof 'Eu sou um VENCEDOR!!!');
+console.log(typeof `Bittencourt`);
+console.log(typeof "Infinity");
+console.log(typeof Infinity);
