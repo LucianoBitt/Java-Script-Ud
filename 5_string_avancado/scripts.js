36d4e62360a4026175a5b859b803011c55d76bc4
Luciano Bittencourt <lucianobitt@hotmail.com>
2023-07-14 11:34:33 -0300

    aulas

--- 5_string_avancado/scripts.js
+++ 5_string_avancado/scripts.js
@@ -1,3 +1,5 @@
 console.log("Primeira linha \n Segunda linha");
-console.log("O meu nome é 'Luciano'");
-log
+console.log("O meu nome é 'Luciano'!");
+console.log('O nome dela é "Stefânia"!');
+console.log(`A multiplicação de 5 por 3 é ${5 * 3}`);
+console.log("O " + "meu " + "nome " + "é " + "Luciano!");
---
+++ 6_booleans/scripts.js
@@ -0,0 +1,4 @@
+console.log(true);
+console.log(false);
+console.log(10 > 2);
+conselo.log(5 < 200);
