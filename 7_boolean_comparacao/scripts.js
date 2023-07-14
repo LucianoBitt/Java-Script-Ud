6adf8dd14b8c5751d656f5580b7eb64b487be6ef
Luciano Bittencourt <lucianobitt@hotmail.com>
2023-07-14 12:35:28 -0300

    boolean

---
+++ 7_boolean_comparacao/scripts.js
@@ -0,0 +1,9 @@
+console.log(1 > 2);
+console.log(5 < 10);
+console.log(3 >= 3);
+console.log(5 <= 4);
+console.log('\n');
+console.log(5 == 4);
+console.log('Luciano' != 'Luciano');
+console.log(3 === '3');
+console.log(4 === 4);
