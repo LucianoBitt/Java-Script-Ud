55019f974342645a8360ce3c6d333fbaef1d61f0
Luciano Bittencourt <lucianobitt@hotmail.com>
2023-07-14 14:38:07 -0300

    ternario

---
+++ 8_booleans_op_logicos/scripts.js
@@ -0,0 +1,16 @@
+console.log(true && true);
+console.log(true && false);
+console.log(false || false);
+console.log(!false);
+console.log("\n");
+console.log(5 > 3 && 3 == 2);
+console.log(5 > 3 || 3 == 1);
+console.log("\n");
+console.log(3 == 3 && "Luciano" == "Luciano");
+console.log("Rafael" == "Maria" || false);
+console.log("\n");
+console.log(!(true && true));
+console.log(!(!(true && true)));
+console.log("\n");
+console.log(true && true);
+console.log(false || false);
