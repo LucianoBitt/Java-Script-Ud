cf1c3c49d78f631b05e346929c33d43c2d3af603
Luciano Bittencourt <lucianobitt@hotmail.com>
2023-07-14 16:15:59 -0300

    aulas

---
+++ 9_booleans_op_ternario/scripts.js
@@ -0,0 +1,6 @@
+console.log(5 > 2 ? 'É sim' : 'É não');
+console.log(5 < 2 ? 'É sim' : 'É não');
+console.log(false ? 5 : 4);
+console.log("Luciano" == "Luciano" ? "Olá, Luciano!" : "Não é o Luciano!");
+console.log("Luciano" == "Bittencourt" ? "Olá, Luciano!" : "Não é o Luciano!");
+console.log('\n');
