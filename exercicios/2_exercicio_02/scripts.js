38ed6ed03a303286805edf472cd2ca6932981902
Luciano Bittencourt <lucianobitt@hotmail.com>
2023-07-18 16:01:08 -0300

    exercicios

---
+++ exercicios/2_exercicio_02/scripts.js
@@ -0,0 +1,3 @@
+console.log(159);
+console.log(15.8);
+console.log((3 * (12 + 4)) / 8);
---
+++ exercicios/4_exercicio_04/scripts.js
@@ -0,0 +1,3 @@
+console.log(3 == 3 && 4 > 1); /* and */
+console.log(4 <= 2 || "luciano" == "Pedro"); /* or */
+console.log(!(2 === 2)); /* not */
