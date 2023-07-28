f3c3ea6dbfe23a12cf351b923641014c556066a0
Luciano Bittencourt <lucianobitt@hotmail.com>
2023-07-28 18:02:42 -0300

    aulas

---
+++ 29_incremetacao_resumida/scripts.js
@@ -0,0 +1,18 @@
+for (let numero = 0; numero < 11; numero += 1) {
+    console.log("O número é: " + numero);
+}
+
+let numero = 5;
+
+while (numero < 55) {
+    console.log(numero);
+    numero += 5
+}
+
+for (let i = 0; i < 11; i++) { // Encrementando
+    console.log("O i é: " + i);
+}
+
+for (let i = 10; i > -1; i--) { // Decrementando
+    console.log("O i é: " + i);
+}
---
+++ 30_switch/scripts.js
@@ -0,0 +1,19 @@
+let nome = "João";
+
+switch (nome) {
+    case "Luciano":
+        console.log("O seu nome é Luciano");
+        break;
+    case "Rafael":
+        console.log("O seu nome é Rafael");
+        break;
+    default:
+        console.log("O nome não foi encontrado!");
+        break;
+}
+
+// if (nome == "Estefania") {
+//     console.log("O nome é Estefania");
+// } else {
+//     console.log("O nome não foi encontrado!");
+// }
