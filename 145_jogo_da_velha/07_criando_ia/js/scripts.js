71b4653381ad454e5f6f0dff41a767a69b117f94
Luciano Bittencourt <lucianobitt@hotmail.com>
2023-10-05 15:31:17 -0300

    jogo da velha

--- 145_jogo_da_velha/07_criando_ia/js/scripts.js
+++ 145_jogo_da_velha/07_criando_ia/js/scripts.js
@@ -26,6 +26,14 @@ for (let i = 0; i < boxes.length; i++) {
             // Computar jogada
             if (player1 == player2) {
                 player1++;
+
+                if (secondPlayer == 'ai-player') {
+
+                    // função executar a jogada
+                    computerPlay();
+                    player2++
+                }
+
             } else {
                 player2++;
             }
@@ -33,6 +41,26 @@ for (let i = 0; i < boxes.length; i++) {
             // checa quem venceu
             checkWinCondition();
         }
+    });
+}
+
+// evento para saber se é 2 players ou IA
+for (let i = 0; i < buttons.length; i++) {
+
+    buttons[i].addEventListener("click", function() {
+
+        secondPlayer = this.getAttribute("id");
+
+        for (let j = 0; j < buttons.length; j++) {
+            buttons[j].style.display = 'none';
+        }
+
+        setTimeout(function() {
+
+            let container = document.querySelector("#container");
+            container.classList.remove("hide");
+
+        }, 500);
     });
 }
 
@@ -229,5 +257,32 @@ function declareWinner(winner) {
     let boxesToRemove = document.querySelectorAll(".box div");
     for (let i = 0; i < boxesToRemove.length; i++) {
         boxesToRemove[i].parentNode.removeChild(boxesToRemove[i]);
+    }
+}
+
+// executar a lógica da jogada da CPU
+function computerPlay() {
+    let cloneO = o.cloneNode(true);
+    counter = 0;
+    filled = 0;
+
+    for (let i = 0; i < boxes.length; i++) {
+        let randomNumber = Math.floor(Math.random() * 5);
+
+        // só preencher se estiver o filho
+        if (boxes[i].childNodes[0] == undefined) {
+            if (randomNumber <= 1) {
+                boxes[i].appendChild(cloneO);
+                counter++;
+                break;
+            }
+            // checagem de quantas estão preenchidas
+        } else {
+            filled++;
+        }
+    }
+
+    if (counter == 0 && filled < 9) {
+        computerPlay();
     }
 }
